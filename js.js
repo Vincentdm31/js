e37a6363bfbf4aa3381c1a1d633d0baf509a717c
Vincentdm31 <vincent.dimarco@ynov.com>
2021-01-12 11:45:28 +0100

    jpp

--- js.js
+++ js.js
@@ -11,26 +11,22 @@ req.onload  = function() {
 
     // Create a div and set class
     var mydiv = document.getElementById('mydiv');
-
     var div = document.createElement("div");
     div.setAttribute("class", "card w100 shadow-1");
-
     var card_header = document.createElement("div");
     card_header.setAttribute("class", "card-header txt-center");
-    
     var card_content = document.createElement("div");
     card_content.setAttribute("class", "card-content txt-center");
-
     var button = document.createElement("a");
     button.setAttribute("class", "btn shadow-1 mx-auto airforce dark-4");
-    
-    // Add some text
+    button.addEventListener("click", function(e) {
+      e.preventDefault();
+      change();
+    });
     card_header.appendChild(document.createTextNode(todo.state));
     card_content.appendChild(document.createTextNode(todo.name));
     button.appendChild(document.createTextNode('Fait'));
 
-    // Add it to the document body
-    
     div.appendChild(card_header);
     div.appendChild(card_content);
     div.appendChild(button);
@@ -44,35 +40,39 @@ var btn = document.getElementById('add');
 
 btn.addEventListener("click", function(e) {
   e.preventDefault();
- test();
+ add();
 });
 
-function test(){
+function add(){
     var input = document.getElementById('input').value;
-
     var mydiv = document.getElementById('mydiv');
-
     var div = document.createElement("div");
     div.setAttribute("class", "card w100 shadow-1");
-
     var card_header = document.createElement("div");
     card_header.setAttribute("class", "card-header txt-center");
-    
     var card_content = document.createElement("div");
     card_content.setAttribute("class", "card-content txt-center");
-
     var button = document.createElement("a");
     button.setAttribute("class", "btn shadow-1 mx-auto airforce dark-4");
-    
-    // Add some text
+    button.addEventListener("click", function(e) {
+      e.preventDefault();
+      change();
+    });
+
     card_header.appendChild(document.createTextNode('à faire'));
     card_content.appendChild(document.createTextNode(input));
     button.appendChild(document.createTextNode('Fait'));
-
-    // Add it to the document body
-    
+  
     div.appendChild(card_header);
     div.appendChild(card_content);
     div.appendChild(button);
     mydiv.appendChild(div);
+}
+
+function change(){
+
+let test = document.querySelector("div").closest(".card-header").innerHTML;
+test.getAttribute('data-value');
+console.log(test);
+  
 }
